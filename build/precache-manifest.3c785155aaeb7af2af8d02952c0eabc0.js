8d7d0ce0382ca3d083f93f859d1fe74e53614172
Jonelle Carless <jonellecarless@Jonelles-MacBook-Pro.local>
2019-03-22 16:20:01 -0400

    first commit

---
+++ build/precache-manifest.3c785155aaeb7af2af8d02952c0eabc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0733ff0426ae5ca98eb5",
+    "url": "/static/css/main.41b66af0.chunk.css"
+  },
+  {
+    "revision": "0733ff0426ae5ca98eb5",
+    "url": "/static/js/main.41497bec.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "ccdf85a272b151ebd001",
+    "url": "/static/js/2.7b7f7f25.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4fde585bcf34db8e11cd4bdc845bed21",
+    "url": "/index.html"
+  }
+];
